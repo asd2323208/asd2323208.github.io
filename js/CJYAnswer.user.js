df9a9c7169899efef25cf457fb84e914addabf0a
asd2323208 <39020811+asd2323208@users.noreply.github.com>
2020-04-22 12:28:52 +0800

    Update CJYAnswer.user.js

--- js/CJYAnswer.user.js
+++ js/CJYAnswer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CJYAnswer
 // @namespace    asd2323208.github.io/
-// @version      0.2
+// @version      0.3
 // @description  别看了，用就是了。
 // @author       DANIAO
 // @run-at       document-body
@@ -15,12 +15,12 @@
 
     document.getElementById("type").value=0
 
-    var scriptHtml='function getAnswerManually(){function getAnswer(){var question = document.getElementById("questioninput").value;$.ajax({type: "GET",url: "http://daniao.phpnet.us/?q="+question,dataType: "jsonp",success: function(data) {console.log(data+"|ok");document.getElementById("answerArea").value=data}});}getAnswer()}'
+    var scriptHtml='function getAnswerManually(){function getAnswer(){var question = document.getElementById("questioninput").value;$.ajax({type: "GET",url: "http://daniao.phpnet.us/?q="+question,dataType: "jsonp",success: function(data) {console.log(data+"|ok");document.getElementById("answerArea").value=""}});}getAnswer()}'
     var g = document.createElement('script');
     var s = document.getElementsByTagName('script')[0];
     g.text = scriptHtml
     s.parentNode.insertBefore(g, s);
-    document.body.innerHTML+='<div id="getAnswerManually" style="position: fixed;top: 100px;left: 0px;padding: 10px;background: rgb(255, 255, 255);border-radius: 4px;"><p style="color:red;">输入题目(请耐心等待，不要重复点击！)</p><button onclick="getAnswerManually()">查询答案</button><p>  </p><textarea wrap="off" cols="45" rows="4" style="overflow: auto; border-radius: 4px; margin: 0px; width: 270px; height: 50px;" id="questioninput"></textarea><p>  </p><p>答案如下</p><textarea wrap="off" cols="45" rows="8" style="overflow: auto; border-radius: 4px; margin: 0px; width: 270px; height: 152px;" id="answerArea"></textarea><br><span style="font-size:0.7em;padding-left: 75%;"><a id="myLink">By DANIAO</a></span></div>'
+    document.body.innerHTML+='<div id="getAnswerManually" style="position: fixed;top: 100px;left: 0px;padding: 10px;background: rgb(255, 255, 255);border-radius: 4px;"><p style="color:red;">接口又坏了，下学期再来吧。</p><button onclick="getAnswerManually()">查询答案</button><p>  </p><textarea wrap="off" cols="45" rows="4" style="overflow: auto; border-radius: 4px; margin: 0px; width: 270px; height: 50px;" id="questioninput"></textarea><p>  </p><p>答案如下</p><textarea wrap="off" cols="45" rows="8" style="overflow: auto; border-radius: 4px; margin: 0px; width: 270px; height: 152px;" id="answerArea"></textarea><br><span style="font-size:0.7em;padding-left: 75%;"><a id="myLink">By DANIAO</a></span></div>'
     document.getElementById("myLink").setAttribute('href','https://github.com/asd2323208/asd2323208.github.io/');
     document.getElementById("myLink").setAttribute('target','_blank');
 
